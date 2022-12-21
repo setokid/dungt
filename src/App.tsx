34efc0a809c114e18412c6cbcaee5ac7a0d69199
setokid <dungtran232000@gmail.com>
2022-12-22 01:11:46 +0700

    final

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios'
+import PokemonColection from './component/PokemonColection';
+import { Pokemon, Detail } from './interface'
+
+interface Pokemons {
+  name: string;
+  url: string;
+}
+
+
+
+const App: React.FC = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([])
+  const [nextUrl, setNextUrl] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(true)
+  const [viewDetail, setDetail] = useState<Detail>({
+    id: 0,
+    isOpened: false,
+  })
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const res = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20"
+      );
+      setNextUrl(res.data.next);
+      res.data.results.forEach(async (pokemon: Pokemons) => {
+        const poke = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        setPokemons((p) => [...p, poke.data]);
+        setLoading(false);
+      });
+    };
+    getPokemon();
+  }, []);
+  const nextPage = async () => {
+    setLoading(true);
+    let res = await axios.get(nextUrl);
+    setNextUrl(res.data.next);
+    res.data.results.forEach(async (pokemon: Pokemons) => {
+      const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+      setPokemons((p) => [...p, poke.data])
+      setLoading(false);
+    })
+  }
+
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="pokemon-header">Pokemon</header>
+        <PokemonColection pokemons={pokemons} viewDetail={viewDetail} setDetail={setDetail} />
+        {viewDetail.isOpened && (
+          <div className="btn">
+            <button onClick={nextPage}>{loading ? "Loading..." : "Load more"}{" "}</button>
+          </div>)}
+      </div>
+    </div >
+  );
+}
+
+export default App;
